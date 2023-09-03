3bb971eafc6443b51656d88acfecb6b9e72e7ca6
mazin-majdy <kingmazin01@gmail.com>
2023-09-03 15:11:05 +0300

    new task

--- app/Listeners/PostInFileLog.php
+++ app/Listeners/PostInFileLog.php
@@ -31,9 +31,5 @@ class PostInFileLog
             'country' => $downloadDetails['country'],
             'file_id' => $downloadDetails['file_id'],
         ]);
-        //     'user_id' => $file->user_id,
-        //     'user_id' => $classwork->user_id,
-        //     'content' => $content,
-        // ]);
     }
 }
